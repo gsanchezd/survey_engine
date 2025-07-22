82e1951b14c66f7217867ecfd37d8a77dd3e62e4
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2025-07-22 17:51:27 -0400

    Add UUID column to surveys for unique identification

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_22_203637) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_214044) do
   create_table "survey_engine_answer_options", force: :cascade do |t|
     t.integer "answer_id", null: false
     t.integer "option_id", null: false
