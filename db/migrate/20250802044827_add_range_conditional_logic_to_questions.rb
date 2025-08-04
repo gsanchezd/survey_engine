07828e97d2a352b79a875734595dab06e151ddf6
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2025-08-03 20:20:50 -0400

    fix migtrations problems

--- db/migrate/20250802044827_add_range_conditional_logic_to_questions.rb
+++ db/migrate/20250802044827_add_range_conditional_logic_to_questions.rb
@@ -1,9 +1,9 @@
-class AddRangeConditionalLogicToQuestions < ActiveRecord::Migration[8.0]
+class AddRangeConditionalLogicToQuestions < ActiveRecord::Migration[7.1]
   def change
     add_column :survey_engine_questions, :conditional_operator_2, :string
     add_column :survey_engine_questions, :conditional_value_2, :decimal
     add_column :survey_engine_questions, :conditional_logic_type, :string, default: 'single'
-    
+
     # Add index for performance on the new logic type field
     add_index :survey_engine_questions, :conditional_logic_type
   end
