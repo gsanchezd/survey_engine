dec6aeacc83ca7269c2d89181302f7313a4474f6
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2025-08-04 13:34:19 -0400

    Dummy app shouldnt have their own migrations

--- test/dummy/db/migrate/20250804155124_add_questions_count_to_survey_templates.survey_engine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from survey_engine (originally 20250801192218)
-class AddQuestionsCountToSurveyTemplates < ActiveRecord::Migration[7.1]
-  def change
-    add_column :survey_engine_survey_templates, :questions_count, :integer, default: 0, null: false
-  end
-end
